1f8dfa5284c0586aea4472ed1921c1468b6770db
Leda Themis <me@leda.dev>
2026-01-23 22:00:46 +0300

    feat: protect monitor route

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,46 +6,43 @@ import { playground } from "@colyseus/playground";
  * Import your Room files
  */
 import { PlayerRoom } from "./rooms/PlayerRoom";
+import expressBasicAuth from "express-basic-auth";
 
 export default config({
+  initializeGameServer: (gameServer) => {
+    /**
+     * Define your room handlers:
+     */
+    gameServer.define("central", PlayerRoom);
+  },
 
-    initializeGameServer: (gameServer) => {
-        /**
-         * Define your room handlers:
-         */
-        gameServer.define('central', MyRoom);
+  initializeExpress: (app) => {
+    const basicAuthMiddleware = expressBasicAuth({
+      users: {
+        admin: process.env.ADMIN_PASSWORD,
+      },
+      challenge: true,
+    });
 
-    },
-
-    initializeExpress: (app) => {
-        /**
-         * Bind your custom express routes here:
-         * Read more: https://expressjs.com/en/starter/basic-routing.html
-         */
-        app.get("/hello_world", (req, res) => {
-            res.send("It's time to kick ass and chew bubblegum!");
-        });
-
-        /**
-         * Use @colyseus/playground
-         * (It is not recommended to expose this route in a production environment)
-         */
-        if (process.env.NODE_ENV !== "production") {
-            app.use("/", playground());
-        }
-
-        /**
-         * Use @colyseus/monitor
-         * It is recommended to protect this route with a password
-         * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
-         */
-        app.use("/monitor", monitor());
-    },
-
-
-    beforeListen: () => {
-        /**
-         * Before before gameServer.listen() is called.
-         */
+    /**
+     * Use @colyseus/playground
+     * (It is not recommended to expose this route in a production environment)
+     */
+    if (process.env.NODE_ENV !== "production") {
+      app.use("/", playground());
     }
+
+    /**
+     * Use @colyseus/monitor
+     * It is recommended to protect this route with a password
+     * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
+     */
+    app.use("/monitor", basicAuthMiddleware, monitor());
+  },
+
+  beforeListen: () => {
+    /**
+     * Before before gameServer.listen() is called.
+     */
+  },
 });
